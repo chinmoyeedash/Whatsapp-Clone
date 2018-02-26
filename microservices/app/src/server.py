9bf488001c15a7b273344095613e53d6c19c71e4
chinmoyeedash <chinmoyeedash@gmail.com>
2018-02-27 00:28:39 +0530

    random

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -1,7 +1,14 @@
 from src import app
-from flask import render_template
-# from flask import jsonify
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+if __name__ == '__main__':
+    socketio.run(app)
+	
 @app.route("/")
 def home():
     return render_template('index.html')
