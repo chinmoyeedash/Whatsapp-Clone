a99d7f3aff1ff8aa3c5327c68611b89c1e3efe7f
chinmoyeedash <chinmoyeedash@gmail.com>
2018-02-27 00:55:33 +0530

    random

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
+app.config['SECRET_KEY'] = 'mysecret'
 socketio = SocketIO(app)
 
 if __name__ == '__main__':
