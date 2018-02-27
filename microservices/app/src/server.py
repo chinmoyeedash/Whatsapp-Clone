a64a6a1ef8db37559f44362e374ace1e5cd11276
chinmoyeedash <chinmoyeedash@gmail.com>
2018-02-27 12:15:00 +0530

    random

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -5,9 +5,51 @@ from flask_socketio import SocketIO
 app.config['SECRET_KEY'] = 'mysecret'
 socketio = SocketIO(app)
 
+print("STARTING")
+sockets = []
+mobile = []
+clients = []
+#sockets[0] = socketio
+
+@socketio.on('myConnect')
+def handleconnect(json):
+    print('in MYCONNECT')
+    print(str(json))
+    mobile.append(json['fromMobile'])
+    print(mobile)
+    sockets.append(socketio)
+    print(sockets)
+
+    clients.append(request.sid)
+
+    
+#   print('Message is ' + msg)
+#   print('from mobile' + str(fromMobile))
+#   sockets.append(socketio)
+
+@socketio.on('myMessage')
+def handlemessage(json):
+    print('in MYMESSAGE',str(json))
+    print('the message',json['msg'])
+    tp_index = mobile.index(json['toMobile'])
+    print('tp_index=',tp_index)
+#   socketio.to(sockets[tp_index]).emit('message',json['msg'])
+    emit('message',json['msg'],room=clients[tp_index])
+#   print('Message is ' + msg)
+#   print('from mobile' + str(fromMobile))
+#   print('to Mobile' + str(toMobile))
+#   socketio.emit()
+#   send(msg,broadcast=True)
+
+@socketio.on('message')
+def handlemessage2(msg):
+    print('in message handler '+msg)
+	
 if __name__ == '__main__':
     socketio.run(app)
 	
 @app.route("/")
 def home():
     return render_template('index.html')
+
+	
