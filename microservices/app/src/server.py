03a4b599a8c2cba1995ef72b34f824d1f7ed0e83
chinmoyeedash <chinmoyeedash@gmail.com>
2018-03-04 00:52:24 +0530

    checking old code

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -1,71 +1,49 @@
-from src import app
-from flask import render_template, session, request
-from flask_socketio import SocketIO, send, emit
+from flask import Flask,request
+from flask_socketio import SocketIO,send,emit
 
+app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
-params = {
-	'ping_timeout': 30000,
-	'ping_interval': 30000
-}
-socketio = SocketIO(app, **params)
+socketio = SocketIO(app)
 
-print("STARTING NOW")
-
+print("ATARTING")
 sockets = []
 mobile = []
 clients = []
-
-# #sockets[0] = socketio
+#sockets[0] = socketio
 
 @socketio.on('myConnect')
 def handleconnect(json):
-    print('in MYCONNECT')
-    print(str(json))
-    fromuserid = json['fromuserid']
-    if fromuserid not in mobile:
-        mobile.append(fromuserid)
-    print(mobile)
-    clients.append(request.sid)
-    print(clients)
-    
-#     sockets.append(socketio)
-#     print(sockets)
-    
+	print('in MYCONNECT')
+	print(str(json))
+	mobile.append(json['fromMobile'])
+	print(mobile)
+	sockets.append(socketio)
+	print(sockets)
 
-    
-#   print('Message is ' + msg)
-#   print('from mobile' + str(fromMobile))
-#   sockets.append(socketio)
+	clients.append(request.sid)
+
+	
+#	print('Message is ' + msg)
+#	print('from mobile' + str(fromMobile))
+#	sockets.append(socketio)
 
 @socketio.on('myMessage')
 def handlemessage(json):
-    print('in MYMESSAGE',str(json))
-    print('the message',json['msg_text'])
-    tp_index = mobile.index(json['receiver_id'])
-    print('tp_index=',tp_index)
-    emit('message',json['msg_text'],room=clients[tp_index])
-    print('Message is ' + json['msg_text'])
-    print('sent time ' + json['sent_time'])
-    print('sender_id' + json['sender_id'])
-    print('receiver_id' + json['receiver_id'])
-#   socketio.to(sockets[tp_index]).emit('message',json['msg'])
-    
-    #emit('message',json['msg'])
-
-   
-#   socketio.emit()
-#   send(msg,broadcast=True)
+	print('in MYMESSAGE',str(json))
+	print('the message',json['msg'])
+	tp_index = mobile.index(json['toMobile'])
+	print('tp_index=',tp_index)
+#	socketio.to(sockets[tp_index]).emit('message',json['msg'])
+	emit('message',json['msg'],room=clients[tp_index])
+#	print('Message is ' + msg)
+#	print('from mobile' + str(fromMobile))
+#	print('to Mobile' + str(toMobile))
+#	socketio.emit()
+#	send(msg,broadcast=True)
 
 @socketio.on('message')
 def handlemessage2(msg):
-    print('in message handler '+msg)
-	
-if __name__ == '__main__':
-    # socketio.run(app)
-    socketio.run(app, host='https://app.crawfish92.hasura-app.io')
-	
-@app.route("/")
-def home():
-    return render_template('chatFront.html')
+	print('in message handler '+msg)
 
-	
+if __name__ == '__main__':
+	socketio.run(app)
