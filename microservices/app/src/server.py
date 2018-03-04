5523efb3868b2e2e7647e1d8780410f5f7a06dbe
chinmoyeedash <chinmoyeedash@gmail.com>
2018-03-05 01:53:52 +0530

    setting chatFront

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -18,9 +18,9 @@ def handleconnect(json):
     print('in MYCONNECT')
     print(str(json))
     fromuserid = json['fromuserid']
-    #if fromuserid not in mobile:
-    mobile.append(fromuserid)
-    clients.append(request.sid)
+    if fromuserid not in mobile:
+        mobile.append(fromuserid)
+        clients.append(request.sid)
     print(mobile)
     print(clients)
 #     sockets.append(socketio)
@@ -39,8 +39,8 @@ def handlemessage(json):
     print('the message',json['msg_text'])
     tp_index = mobile.index(json['receiver_id'])
     print('tp_index=',tp_index)
-    emit('message',json['msg'])
-#    emit('message',json['msg_text'],room=clients[tp_index])
+#    emit('message',json['msg'])
+    emit('message',json['msg_text'],room=clients[tp_index])
     print('Message is ' + json['msg_text'])
     print('sent time ' + json['sent_time'])
     print('sender_id' + json['sender_id'])
